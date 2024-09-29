d9f682292e6396780c4e9e2e51f5f4c6a66d2625
Emilio Reato <reatoemilio@gmail.com>
2024-09-29 19:22:40 +0200

    mesaggee

--- clases.py
+++ clases.py
@@ -1,18 +1,31 @@
 import pygame
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))  # sets the current directory to the file's directory
 
 
 class Piece:
 
-    hp = 100
-    mana = 100
-    agility = 100
-    defense = 100
-    damage = 100
+    init_hp = 100
+    init_mana = 100
+    init_agility = 100
+    init_defense = 100
+    init_damage = 100
 
-    def __init__(self, x, y, rad):
+    pieces_dimension = 0
+
+    def __init__(self, x, y):
         self.pos_x = x
         self.pos_y = y
-        self.rad = rad
+        # self.rad = rad
+        """global init_hp, init_mana, init_agility, init_defense, init_damage
+        self.hp = init_hp
+        self.mana = init_mana
+        self.agility = init_agility
+        self.defense = init_defense
+        self.damage = init_damage"""
+        if Piece.pieces_dimension is 0:
+            from main import height  # Importación diferida
+            Piece.pieces_dimension = height // 14
 
     def mover(self, move_x, move_y, change_mana):
         self.pos_x += move_x
@@ -21,17 +34,30 @@ class Piece:
             if self.mana > 0:
                 self.mana -= 1
 
-    def dibujar(self, screen, color, pos):
-        pygame.draw.circle(screen, color, pos, self.rad)
+    def draw(self, screen, img, pos=0):
+        print(self.pos_x, self.pos_y)
+        if pos:
+            screen.blit(img, (pos[0]-Piece.pieces_dimension//2, pos[1]-Piece.pieces_dimension//2))
+        else:
+            screen.blit(img, (self.pos_x-Piece.pieces_dimension//2, self.pos_y-Piece.pieces_dimension//2))
+        # pygame.draw.circle(screen, color, pos, self.rad)
+
+    # def get_center(self,image):
 
     def is_clicked(self, mouse_pos, pos):
-        distancia = ((mouse_pos[0] - pos[0]) ** 2 + (mouse_pos[1] - pos[1]) ** 2) ** 0.5  # Calcular la distancia entre el clic y el centro del círculo
-        return distancia <= self.rad  # Devuelve True si el clic está dentro del círculo
 
-    # def get_center(self):
+        distancia = ((pos[0] - mouse_pos[0]) ** 2 + (pos[1] - mouse_pos[1]) ** 2) ** 0.5  # Calcular la distancia entre el cli
+        return distancia <= Piece.pieces_dimension//2  # Devuelve True si el clic está dentro del círculo
 
 
 class Mage(Piece):
-    def __init__(self, x, y, rad, mana):
-        super().__init__(x, y, rad)
+
+    red_mage_image = pygame.image.load("resources\\red_mage.png")  # .convert()
+    blue_mage_image = pygame.image.load("resources\\blue_mage.png")  # .convert()
+
+    def __init__(self, x, y, mana):
+        super().__init__(x, y)
         self.mana = mana
+
+        Mage.red_mage_image = pygame.transform.smoothscale(Mage.red_mage_image, (Piece.pieces_dimension, Piece.pieces_dimension))
+        Mage.blue_mage_image = pygame.transform.smoothscale(Mage.blue_mage_image, (Piece.pieces_dimension, Piece.pieces_dimension))
--- main.py
+++ main.py
@@ -1,88 +1,96 @@
-import clases
-
 from win32con import ENUM_CURRENT_SETTINGS
 from win32api import EnumDisplaySettings
-import serial
 import pygame
 import time
 import numpy
 import sys
-import serial.tools.list_ports
 import pygame_gui
 import os
-import webbrowser
-# do not delete this eventhough it is not used, for some reason it increases the render quality
-from pyautogui import press
+import pyautogui
+from pyautogui import press  # do not delete this eventhough it is not used, for some reason it increases the render quality
+import clases
 
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+# SETING THINGS UP
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))  # sets the current directory to the file's directory
 
 pygame.init()  # Inicializar Pygame
 
-info = pygame.display.Info()  # Seteando las dimesiones de la ventana 16:9
-screen_height = info.current_h
-screenratio = 1.4  # 1.5 is the ratio screen/window
-height = round(screen_height/screenratio)
-width = height*(1920/1080)
-screen = pygame.display.set_mode([width, height])
+_, screen_height = pyautogui.size()  # gets the current resolution
+screenratio = 1  # is the ratio screen/window
+height = round(screen_height/screenratio)  # reduces the height
+width = round(height*(16/9))  # sets the aspect ratio to 16:9
+screen = pygame.display.set_mode([width, height])  # sets window resolution
 
-pygame.display.set_caption("Gambit")  # ponemos un titulo
-
-bkg_img = pygame.image.load("resources\\background.png").convert()
-bkg_img = pygame.transform.smoothscale(bkg_img, (width, height))
+pygame.display.set_caption("Gambit")  # set a window title
 
 
-# Cargar una imagen (reemplaza con la ruta de tu imagen)
-# image_path = "ruta_a_tu_imagen.png"
-# image = pygame.image.load(image_path)
+def load_images():
 
-# Escalar la imagen si es necesario para que se ajuste a la ventana
-# image = pygame.transform.scale(image, (width, height))
+    global bkg_img, cursor_default, close_button, close_button_rect, settings_button, settings_button_rect
 
-# Configurar el reloj para controlar los FPS
-timer = pygame.time.Clock()
-dev_mode = EnumDisplaySettings(None, ENUM_CURRENT_SETTINGS)  # get the fps OS setting
-timer.tick(dev_mode.DisplayFrequency)  # fps of the window
-# fps config for pygame_gui
+    bkg_img = pygame.image.load("resources\\background.png").convert()  # load some images, converts it for optimization and then scales them.
+    bkg_img = pygame.transform.smoothscale(bkg_img, (width, height))
 
-pygame.mouse.set_visible(True)  # both needed for set mouse in virtual mode
-pygame.event.set_grab(False)
+    cursor_default = pygame.image.load("resources\\icons\\cursor_default.png").convert_alpha()
+    cursor_default = pygame.transform.smoothscale(cursor_default, (screen_height * 0.805 // 43, screen_height // 43))
+
+    close_button = pygame.image.load("resources\\icons\\x.png").convert_alpha()
+    close_button = pygame.transform.smoothscale(close_button, (height // 24, height // 24))
+    close_button_rect = close_button.get_rect()
+    close_button_rect.topleft = (height//0.6, height // 25)
+
+    settings_button = pygame.image.load("resources\\icons\\setting.png").convert_alpha()
+    settings_button = pygame.transform.smoothscale(settings_button, (height // 24, height // 24))
+    settings_button_rect = settings_button.get_rect()
+    settings_button_rect.topleft = (height//0.62, height // 25)
+
+
+load_images()
+
+
+timer = pygame.time.Clock()  # create a clock object to set fps
+dev_mode = EnumDisplaySettings(None, ENUM_CURRENT_SETTINGS)  # get the OS's fps setting
+
+pygame.mouse.set_visible(False)  # both needed for set mouse in virtual mode
+pygame.event.set_grab(True)
 
 UI_REFRESH_RATE = timer.tick(dev_mode.DisplayFrequency)/1000
 manager = pygame_gui.UIManager((width, height))
 
-pieza1 = clases.Piece(5, 5, 20)
 
-board = [[""]*8]*8
+pieza1 = clases.Mage(500, 500, 13)
 
-print(board)
+# board = [[""]*8]*8
+# print(board)
 
-color_claro = (255, 255, 255)  # Blanco
+"""color_claro = (255, 255, 255)  # Blanco
 color_oscuro = (0, 0, 0)
+tamano_cuadrado = (height-200) // 8  # Dividir el ancho de la ventana entre 8"""
+
 follow_mouse = False
 
-tamano_cuadrado = (height-200) // 8  # Dividir el ancho de la ventana entre 8
-while True:
 
-    screen.blit(bkg_img, (0, 0))
+def draw():
+    screen.blit(bkg_img, (0, 0))  # display background
+    pieza1.draw(screen, pieza1.blue_mage_image)
+    screen.blit(close_button, (height//0.6, height // 25))
+    screen.blit(settings_button, (height//0.62, height // 25))
 
-    print(pieza1.pos_x, pieza1.pos_y)
+    screen.blit(cursor_default, pygame.mouse.get_pos())  # display cursor
 
-    """for fila in range(8):
-        for columna in range(8):
-            # Alternar colores
-            if (fila + columna) % 2 == 0:
-                color = color_claro
-            else:
-                color = color_oscuro
+    pygame.display.flip()  # Actualizar la pantalla #.update()
 
-            # Dibujar el cuadrado
-            board[fila][columna] = pygame.draw.rect(screen, color, (300+columna * tamano_cuadrado, 100+fila * tamano_cuadrado, tamano_cuadrado, tamano_cuadrado))
-            """
+
+run = True
+while run:  # Main loop
+
+    # print(pieza1.pos_x, pieza1.pos_y)
+
     if follow_mouse:
         pieza1.pos_x, pieza1.pos_y = event.pos
     else:
         pass
-    pieza1_circle = pieza1.dibujar(screen, pygame.Color(50, 50, 10), (pieza1.pos_x, pieza1.pos_y))
 
     # Manejar eventos
     for event in pygame.event.get():
@@ -91,42 +99,43 @@ while True:
                 # Obtener la posición del ratón
                 mouse_pos = event.pos
 
-                # Comprobar si el clic está dentro del cuadrado
-                if pieza1.is_clicked(mouse_pos, (pieza1.pos_x, pieza1.pos_y)):
-                    print("¡Clic en el cuadrado!")
+                if pieza1.is_clicked(mouse_pos, (pieza1.pos_x, pieza1.pos_y)):  # Comprobar si el clic está dentro del circulo
+                    print("pieza clickeada")
                     follow_mouse = True
+
+                elif close_button_rect.collidepoint(mouse_pos):  # check if button was clicked
+                    pygame.quit()
+                    sys.exit()
+
         if event.type == pygame.MOUSEBUTTONUP:
 
             if event.button == 1:
                 follow_mouse = False
-            for fila in board:
+            """for fila in board:
                 for rect in fila:
                     mouse_pos = event.pos
                     print((pieza1.pos_x, pieza1.pos_y))
                     if rect.collidepoint(mouse_pos):
                         pieza1.pos_x, pieza1.pos_y = rect.center
                         print("fifjei")
-                        break
+                        break"""
 
         elif event.type == pygame.KEYDOWN:
             if (pygame.key.name(event.key) == "w"):
-                pieza1.mover(0, 1)
+                pieza1.mover(0, 1, True)
             elif (pygame.key.name(event.key) == "s"):
-                pieza1.mover(0, -1)
+                pieza1.mover(0, -1, True)
             elif (pygame.key.name(event.key) == "a"):
-                pieza1.mover(1, 0)
+                pieza1.mover(1, 0, True)
             elif (pygame.key.name(event.key) == "d"):
-                pieza1.mover(-1, 0)
+                pieza1.mover(-1, 0, True)
+            elif (pygame.key.name(event.key) == "escape"):
+                pygame.quit()
+                sys.exit()
         elif event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-    # Dibujar la imagen en la ventana
-    # screen.blit(image, (0, 0))
+    draw()
 
-    # Actualizar la pantalla
-    pygame.display.flip()
-    # time.sleep(1)
-
-    # Mantener 144 FPS
-    timer.tick(dev_mode.DisplayFrequency)
+    timer.tick(dev_mode.DisplayFrequency)  # set the fps to the maximun possible
